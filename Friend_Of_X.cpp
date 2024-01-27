e3c4a4d50b4fd4e7a890feb6bf67c5a1934b50e5
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2024-01-27 12:40:20 +0530

    Add files via upload

---
+++ Friend_Of_X.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string isFriend(int n, int x, int y, vector<int> &arr) {
+        // code here
+        for(int i:arr)
+            if(i+x==y) return "yes";
+        return "no";
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        int x;
+        scanf("%d",&x);
+        
+        
+        int y;
+        scanf("%d",&y);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        Solution obj;
+        string res = obj.isFriend(n, x, y, arr);
+        
+        cout<<res<<"\n";
+        
+    }
+}
+
+// } Driver Code Ends
